03c3792b6fa43e92f903ae49b6096cd3849f0f71
Najam2008 <122669410+Najam2008@users.noreply.github.com>
2023-10-23 16:47:18 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var bg,bgImg;
+var player ,shooterImg,shooter_shooting;
+var zombie,zombieImg;
+
+var heart1,heart2,heart3;
+var heart1Img,heart2Img,heart3Img;
+
+var zombieGroup;
+
+var score = 0;
+var life = 3;
+var bullets= 70;
+
+var heart1,heart2,heart3;
+
+var gameState =  "fight"
